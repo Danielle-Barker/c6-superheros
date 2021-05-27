39c5243780bc61ee55993ac9d5cf78e47d670e94
danibarker <caseybarker86@gmail.com>
2021-05-27 05:11:48 -0600

    oops

--- server/app.js
+++ server/app.js
@@ -1,41 +1,41 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
 // IMPORT ROUTES
-var superheroRouter = require('./routes/superhero');
+var superheroRouter = require("./routes/superhero");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // USE ROUTES
-app.use('/superhero', superheroRouter);
+app.use("/superhero", superheroRouter);
 
 // serve the react application
-app.use(express.static('../client/build'));
+app.use(express.static("../client/build"));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
-app.use('*',(req,res)=>{
-  res.sendFile(path.join(__dirname,'../client/build/index.html'))
-})
+app.use("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.send('error');
+  res.send("error");
 });
 
 module.exports = app;
